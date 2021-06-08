c5315a9c7147672d8c54a89072f4fc2c4787ae40
shinesha <jodrew_7@hotmail.com>
2021-06-08 18:30:25 +0100

    conditional statement problem

--- Module_Control_Flow.py
+++ Module_Control_Flow.py
@@ -41,3 +41,23 @@ else:
 
 message = "Somebody who is {} years old will pay ${} to ride the bus.".format(age, ticket_price)
 print(message)
+
+
+-----------------------------------------------
+conditional statement
+
+points = 174  # use this input to make your submission
+
+# write your if statement here
+
+if points <= 50:
+    result = "Congratulations! You won a wooden rabbit!"
+elif points <= 150:
+    result = "Oh dear, no prize this time."
+elif points <= 180:
+    result = "Congratulations! You won a wafer-thin mint!"
+else:
+    result = "Congratulations! You won a penguin!"
+
+print(result)
+
