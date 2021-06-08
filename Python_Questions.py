96b65efd927227129f5d040cf06ac200ab22342f
shinesha <jodrew_7@hotmail.com>
2021-06-08 15:45:22 +0100

    tidy up

--- Python_Questions.py
+++ Python_Questions.py
@@ -17,31 +17,32 @@ print(eclipse_dates[-3:])
 
 
 ----------------------------------------------------------
-elements = {'hydrogen': {'number': 1, 'weight': 1.00794, 'symbol': 'H'},
-            'helium': {'number': 2, 'weight': 4.002602, 'symbol': 'He'}}
-elements["hydrogen"]["is_noble_gas"]=False
-elements["helium"]["is_noble_gas"]=True
+def compound_dictionary():
+    elements = {'hydrogen': {'number': 1, 'weight': 1.00794, 'symbol': 'H'},
+                'helium': {'number': 2, 'weight': 4.002602, 'symbol': 'He'}}
+    elements["hydrogen"]["is_noble_gas"]=False
+    elements["helium"]["is_noble_gas"]=True
 
-# todo: Add an 'is_noble_gas' entry to the hydrogen and helium dictionaries
-# hint: helium is a noble gas, hydrogen isn't
+    # todo: Add an 'is_noble_gas' entry to the hydrogen and helium dictionaries
+    # hint: helium is a noble gas, hydrogen isn't
 
-print(elements["hydrogen"]["is_noble_gas"])
-print(elements["helium"]["is_noble_gas"])
+    print(elements["hydrogen"]["is_noble_gas"])
+    print(elements["helium"]["is_noble_gas"])
 
 -------------------------------------------------------------
 def count_unique_words():
-verse = "if you can keep your head when all about you are losing theirs and blaming it on you   if you can trust yourself when all men doubt you     but make allowance for their doubting too   if you can wait and not be tired by waiting      or being lied about  don’t deal in lies   or being hated  don’t give way to hating      and yet don’t look too good  nor talk too wise"
-print(verse, '\n')
+    verse = "if you can keep your head when all about you are losing theirs and blaming it on you   if you can trust yourself when all men doubt you     but make allowance for their doubting too   if you can wait and not be tired by waiting      or being lied about  don’t deal in lies   or being hated  don’t give way to hating      and yet don’t look too good  nor talk too wise"
+    print(verse, '\n')
 
-# split verse into list of words
-verse_list = list(verse.split(" "))
+    # split verse into list of words
+    verse_list = list(verse.split(" "))
 
-print(verse_list, '\n')
+    print(verse_list, '\n')
 
-# convert list to a data structure that stores unique elements
-verse_set = set(verse_list)
-print(verse_set, '\n')
+    # convert list to a data structure that stores unique elements
+    verse_set = set(verse_list)
+    print(verse_set, '\n')
 
-# print the number of unique words
-num_unique = len(verse_set)-1
-print(num_unique, '\n')
+    # print the number of unique words
+    num_unique = len(verse_set)-1
+    print(num_unique, '\n')
