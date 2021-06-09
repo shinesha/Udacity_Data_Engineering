d4142ceb9bd301322b50ba69454cbbc67f066a3f
shinesha <jodrew_7@hotmail.com>
2021-06-09 21:09:51 +0100

    modify userames with range problem

--- Module_Control_Flow.py
+++ Module_Control_Flow.py
@@ -141,4 +141,13 @@ for name in names:
 
 print(usernames)
 
+--------------------------------------------------------
+usernames = ["Joey Tribbiani", "Monica Geller", "Chandler Bing", "Phoebe Buffay"]
+
+# write your for loop here
+for i in range(len(usernames)):
+    usernames[i] = usernames[i].lower().replace(" ", "_")
+
+print(usernames)
+
 
