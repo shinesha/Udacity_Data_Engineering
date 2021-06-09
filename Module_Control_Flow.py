0998357c3d28513753eead1c463bffa2cd55490b
shinesha <jodrew_7@hotmail.com>
2021-06-09 18:34:21 +0100

    guess my answer problem

--- Module_Control_Flow.py
+++ Module_Control_Flow.py
@@ -61,3 +61,26 @@ else:
 
 print(result)
 
+
+--------------------------------------------------------
+# '''
+# You decide you want to play a game where you are hiding
+# a number from someone.  Store this number in a variable
+# called 'answer'.  Another user provides a number called
+# 'guess'.  By comparing guess to answer, you inform the user
+# if their guess is too high or too low.
+
+# Fill in the conditionals below to inform the user about how
+# their guess compares to the answer.
+# '''
+answer = 5
+guess = 5
+
+if guess<answer:
+    result = "Oops!  Your guess was too low."
+elif guess>answer:
+    result = "Oops!  Your guess was too high."
+elif guess==answer:
+    result = "Nice!  Your guess matched the answer!"
+
+print(result)
