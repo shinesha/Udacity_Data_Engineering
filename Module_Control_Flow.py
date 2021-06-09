efe265faa30bfaa235c9b2d0944ee9868191d2d1
shinesha <jodrew_7@hotmail.com>
2021-06-09 19:51:18 +0100

    for loop practice

--- Module_Control_Flow.py
+++ Module_Control_Flow.py
@@ -120,3 +120,12 @@ sentence = ["the", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "do
 # Write a for loop to print out each word in the sentence list, one word per line
 for i in sentence:
     print (i)
+
+------------------------------------------------------
+# Write a for loop using range() to print out multiples of 5 up to 30 inclusive
+
+for num in range(1, 31):
+    if num%5 ==0:
+        print(num)
+
+
